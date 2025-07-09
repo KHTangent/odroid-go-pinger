d80cc9f59f4fdd3aad7de2927610fd92b0f59cab
KHTangent <contact@khtangent.com>
2025-07-09 13:35:42 +0200

    Add hello world with LVGL

--- app/src/main.c
+++ app/src/main.c
@@ -1,10 +1,17 @@
+#include <lvgl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <zephyr/drivers/display.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/zbus/zbus.h>
 
 #include "app/drivers/odroid_go_buttons.h"
+#include "zephyr/device.h"
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+static const struct device* display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 
 void button_callback(const struct zbus_channel* channel) {
 	if (channel != &button_event_channel) {
@@ -28,8 +35,15 @@ ZBUS_CHAN_ADD_OBS(button_event_channel, ZBUS_OBSERVERS(button_observer), 0);
 
 int main(void) {
 	if (!gpio_is_ready_dt(&led)) {
+		printf("Could not init GPIO\n");
 		return 0;
 	}
+	if (!device_is_ready(display)) {
+		printf("Could not init display\n");
+		return 0;
+	}
+
+	display_blanking_off(display);
 
 	int pin_config_error = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (pin_config_error < 0) {
@@ -37,8 +51,12 @@ int main(void) {
 	}
 	gpio_pin_set_dt(&led, 0);
 
+	lv_obj_t* hello_label = lv_label_create(lv_scr_act());
+	lv_label_set_text(hello_label, "Hello, World!");
+
 	while (1) {
-		k_sleep(K_MSEC(100));
+		lv_task_handler();
+		k_sleep(K_MSEC(50));
 	}
 	return 0;
 }
